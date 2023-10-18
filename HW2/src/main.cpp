d11b3f67fc04e60c71495a4bc7a5cf70ab9d713a
jmyapple9 <jmyapple9@gmail.com>
2023-10-18 09:48:07 +0800

    parser without caring the index problem

--- HW2/src/main.cpp
+++ HW2/src/main.cpp
@@ -1,18 +1,182 @@
-#include<iostream>
-#include<string>
-#include<tuple>
-
-
-
+#include "main.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+// #include <utility>
 using namespace std;
 
-tuple<string, string> eatArg(int argc, char *argv[]){
-    for(int i = 0; i<argc; i++){
+ifstream in_file;
+vector<pair<int, int>> TechA; // index is LibCell number-1 (ex. MC1 will be store at TechA[0])
+vector<pair<int, int>> TechB; // pair<w,t>
+vector<cell *> cellArray;
+vector<net *> netArray;
 
+int NumTechs;
+int NumLibCells;
+
+die Die;
+
+pair<string, string> eatArg(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Input format should be the following." << endl;
+        cout << "eg.  ./hw2 ../testcase/public1.txt ../output/public1.out" << endl;
+        cout << "exiting..." << endl;
+        exit(1);
+    }
+    return {argv[1], argv[2]};
+}
+
+void getIss(istringstream &iss, string line)
+{
+    iss.clear();
+    iss.str("");
+    getline(in_file, line);
+    iss.str(line);
+}
+
+void check(){
+    cout << "checking net Array:" << endl;
+    for (int i = 1; i < netArray.size(); i++)
+    {
+        cout << "In net " << i << " having " << netArray[i]->numCells << " cells:" << endl;
+        for(int c:netArray[i]->cells){
+            cout << c << " ";
+        }
+        cout << endl;
+    }
+    cout << "\nchecking cell Array:" << endl;
+    for (int i = 1; i < cellArray.size(); i++)
+    {
+        cout << "In cell " << i << " having " << cellArray[i]->nets.size() << " nets:" << endl;
+        for(int n:cellArray[i]->nets){
+            cout << n << " ";
+        }
+        cout << endl;
     }
 }
 
-int main(int argc, char *argv[]){
+void parser(string testcasePath)
+{
+    in_file.open(testcasePath);
+    // ifstream in_file(testcasePath);
+
+    if (in_file.fail())
+    {
+        cout << "Fail opening file: " << testcasePath << endl;
+        exit(1);
+    }
+
+    istringstream iss;
+    string line, s1, s2, s3;
+    int t1, t2, t3;
+
+    // NumTechs
+    getIss(iss, line);
+    iss >> s1 >> NumTechs;
+
+    // Tech TA's LibCell
+    getIss(iss, line);
+    iss >> s1 >> s2 >> NumLibCells;
+
+    TechA.emplace_back(make_pair(-1, -1)); // put dummy node at index 0
+    for (int i = 0; i < NumLibCells; i++)
+    {
+        getIss(iss, line);
+        iss >> s1 >> s2 >> t1 >> t2;
+        TechA.emplace_back(make_pair(t1, t2));
+    }
+
+    // Tech TB's LibCell, if exist
+    if (NumTechs == 2)
+    {
+        getIss(iss, line);
+        iss >> s1 >> s2 >> NumLibCells;
+
+        TechB.emplace_back(make_pair(-1, -1)); // put dummy node at index 0
+        for (int i = 0; i < NumLibCells; i++)
+        {
+            getIss(iss, line);
+            iss >> s1 >> s2 >> t1 >> t2;
+            TechB.emplace_back(make_pair(t1, t2));
+        }
+    }
+
+    // eat newline
+    getline(in_file, line);
+
+    // Die info
+    getIss(iss, line);
+    iss >> s1 >> Die.w >> Die.h;
+
+    getIss(iss, line);
+    iss >> s1 >> s2 >> Die.utilA;
+    Die.techA = s2[1];
+    Die.utilA = Die.utilA / 100.;
+
+    getIss(iss, line);
+    iss >> s1 >> s2 >> Die.utilB;
+    Die.techB = s2[1];
+    Die.utilB = Die.utilB / 100.;
+
+    // eat newline
+    getline(in_file, line);
+
+    // build cell array
+    getIss(iss, line);
+    iss >> s1 >> t1;
+    cellArray.emplace_back(new cell());
+    while (t1--)
+    {
+        getIss(iss, line);
+        iss >> s1 >> s2 >> s3;
+        s3 = s3.substr(2);
+        cellArray.emplace_back(new cell(stoi(s3)));
+    }
+
+    // eat newline
+    getline(in_file, line);
+
+    // Build netArray
+    getIss(iss, line);
+    iss >> s1 >> t1;
+    netArray.emplace_back(new net());
+    for (int net_id = 1; net_id <= t1; net_id++)
+    {
+        getIss(iss, line);
+        iss >> s1 >> s2 >> t2;
+
+        net *n = new net(t2);
+        while (t2--)
+        {
+            getIss(iss, line);
+            iss >> s1 >> s2;
+            int cell_id = stoi(s2.substr(1));
+            n->cells.insert(cell_id);
+            // Also build cellArray
+            cellArray[cell_id]->nets.insert(net_id);
+        }
+        netArray.emplace_back(n);
+    }
+
+    // check();
+
+    in_file.close();
+}
+int main(int argc, char *argv[])
+{
     auto [testcasePath, outputPath] = eatArg(argc, argv);
-    
+    parser(testcasePath);
+
+    // for (auto item : TechA)
+    // {
+    //     cout << item.first << " " << item.second << endl;
+    // }
+    // for (auto item : TechB)
+    // {
+    //     cout << item.first << " " << item.second << endl;
+    // }
 }
---
+++ HW2/src/main.hpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <set>
+
+
+using namespace std;
+
+class die;
+class cell;
+class cellNum;
+class net;
+// class netNum;
+
+
+class die{
+public:
+    int w, h;
+    float utilA, utilB;
+    char techA, techB;
+};
+
+class cell{
+public:
+    set<int> nets;
+    int lib;
+    cellNum* c;
+    cell(){ // dummy node constructor
+        c = nullptr;
+        lib = -1;
+    }
+    cell(int _lib){
+        lib = _lib;
+    }
+};
+
+class cellNum{
+public:
+    cellNum *prev, *next;
+    int id;
+};
+
+class net{
+public:
+    set<int> cells;
+    int numCells;
+    net(){ // dummy node constructor
+        numCells = -1;
+    }
+    net(int _numCells){
+        numCells = _numCells;
+    }
+};
+
+// class netNum{
+// public:
+//     netNum *prev, *next;
+//     int id;
+// };
